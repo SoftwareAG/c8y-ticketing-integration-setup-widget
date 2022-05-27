63c037fab62830b8005b5dcdff2fdac955b5eee1
Kalpshekhar Gupta <kagu@softwareag.com>
2022-05-27 16:09:12 +0100

    Fixed local dev issue due to new angular version.

--- app.module.ts
+++ app.module.ts
@@ -3,7 +3,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule as NgRouterModule } from "@angular/router";
 import { UpgradeModule as NgUpgradeModule } from "@angular/upgrade/static";
 import { CoreModule, HOOK_COMPONENTS, RouterModule } from "@c8y/ngx-components";
-import { DashboardUpgradeModule, UpgradeModule, HybridAppModule, UPGRADE_ROUTES } from "@c8y/ngx-components/upgrade";
+import { DashboardUpgradeModule, UpgradeModule, HybridAppModule, UPGRADE_ROUTES, ng1Modules } from "@c8y/ngx-components/upgrade";
 import { AssetsNavigatorModule } from "@c8y/ngx-components/assets-navigator";
 import { CockpitDashboardModule } from "@c8y/ngx-components/context-dashboard";
 import { ReportsModule } from "@c8y/ngx-components/reports";
@@ -16,18 +16,18 @@ import { PaginationModule } from "ngx-bootstrap/pagination";
 @NgModule({
   imports: [
     BrowserAnimationsModule,
-        RouterModule.forRoot(),
-        NgRouterModule.forRoot([...UPGRADE_ROUTES], { enableTracing: false, useHash: true }),
-        CoreModule.forRoot(),
-        AssetsNavigatorModule,
-        ReportsModule,
-        NgUpgradeModule,
-        DashboardUpgradeModule,
-        CockpitDashboardModule,
-        SensorPhoneModule,
-        UpgradeModule,
-        ChartsModule,
-        PaginationModule
+    RouterModule.forRoot(),
+    NgRouterModule.forRoot([...UPGRADE_ROUTES], { enableTracing: false, useHash: true }),
+    CoreModule.forRoot(),
+    AssetsNavigatorModule,
+    ReportsModule,
+    NgUpgradeModule,
+    DashboardUpgradeModule,
+    CockpitDashboardModule,
+    SensorPhoneModule,
+    UpgradeModule,
+    ChartsModule,
+    PaginationModule
   ],
   declarations: [CumulocityTicketingIntegrationSetupWidget, CumulocityTicketingIntegrationSetupWidgetConfig],
   entryComponents: [CumulocityTicketingIntegrationSetupWidget, CumulocityTicketingIntegrationSetupWidgetConfig],
@@ -52,7 +52,13 @@ import { PaginationModule } from "ngx-bootstrap/pagination";
   }, ThemeService],
 })
 export class AppModule extends HybridAppModule {
+  //ng1Modules = ng1Modules;
+  //protected upgrade: NgUpgradeModule;
   constructor(protected upgrade: NgUpgradeModule) {
     super();
   }
+  /*ngDoBootstrap(): void {
+    (window as any).bootstrap();
+    this.upgrade.bootstrap(document.getElementById('app'), this.ng1Modules, { strictDi: false});
+  }*/
 }
